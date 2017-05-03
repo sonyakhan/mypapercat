0997d6f25274cdd2a18e5357541030c60e2e8484
Sonya Khan <sonya.khan.cs@gmail.com>
2017-05-02 22:46:38 -0400

    add start file

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-$('#mainNav').affix({
-  offset: {
-    top: 100
-  }
-});
-
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-        $(this).collapse('hide');
-    }
-});
-
-if($(window).width() >= 1024){
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('.navbar-brand>img').addClass('navbar-logo');
-      $('.menu').removeClass('enlargen');
-    } else {
-      $('.navbar-brand>img').removeClass('navbar-logo');
-      $('.menu').addClass('enlargen');
-    }
-  });
-}
-
 // $(".nav a").on("click", function(){
 //    $(".nav").find(".active").removeClass("active");
 //    $(this).parent().addClass("active");
@@ -177,3 +153,28 @@ joelApp.controller('aceController', function($scope) {
 joelApp.controller('alleyController', function($scope) {
   $scope.message = 'alley';
 });
+
+
+$('#mainNav').affix({
+  offset: {
+    top: 100
+  }
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+if($(window).width() >= 1024){
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.navbar-brand>img').addClass('navbar-logo');
+      $('.menu').removeClass('enlargen');
+    } else {
+      $('.navbar-brand>img').removeClass('navbar-logo');
+      $('.menu').addClass('enlargen');
+    }
+  });
+}
